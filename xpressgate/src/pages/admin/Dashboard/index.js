2a13f61c7d445a19b941eacc4355b7b072ee8129
Gouse <gouse.mohiddin@axzora.com>
2022-11-24 12:18:24 +0530

    setup for super admin

---
+++ xpressgate/src/pages/admin/Dashboard/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import HeaderSection from '../../../components/GuardModule/Utils/HeaderSection';
+import LogOut from '../../../components/GuardModule/Utils/LogOut';
+import Header from '../../../components/Layout/Header';
+
+const AdminDashboard = () => {
+
+    /*
+    SAMPLE USAGE OF THE API
+    
+    useEffect(async () => {
+
+        await fetchUser();
+
+
+
+    }, [])*/
+
+
+
+    return (
+        <>
+            <Header />
+
+        </>
+    )
+}
+
+export default AdminDashboard
+
