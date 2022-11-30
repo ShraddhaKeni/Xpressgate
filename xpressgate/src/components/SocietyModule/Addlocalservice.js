ee931139d69b8b0dcda5fd9e4cec042a78ba3771
Shraddha <shraddha.keni@axzora.com>
2022-11-30 11:18:36 +0530

    add local services page updated

--- xpressgate/src/components/SocietyModule/Addlocalservice.js
+++ xpressgate/src/components/SocietyModule/Addlocalservice.js
@@ -1,101 +1,72 @@
-import React from "react";
-import "../SocietyModule/Addlocalservice.css";
-import LogOut from './Utils/LogOut'
+import React, { useEffect, useState } from 'react';
+import './Addlocalservice.css';
 import { Button } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
-
-
+import axios from 'axios';
+import Societyheader from './Utils/Societyheader';
+import { Link } from 'react-router-dom';
 
 const Addlocalservice = () => {
+
   return (
-    <div className="addguestcontainer1">
-      <div id="headersection1">
-      <div id="addflatsection">
-        <div className="addflatheadersection">
-          <div id="aflogo"><img src="/images/loginlogo.svg" alt="header logo" /></div>
-          <div id="afsociety"><label>Society</label></div>
-          <div id="afspace"></div>
-          <div id="afnotification"><a href="abc"><img src="/images/notification.svg" alt="notificationicon" /></a></div>
-          <div id="afsetting"><a href="abc"><img src="/images/setting.svg" alt="settingicon" /></a></div>
-          <div id="aflogoutbutton"><LogOut /></div>
-        </div>
+    <div className="alscontainer">
+      <div id="alssection">
+        <Societyheader />
       </div>
-      </div>
-      <div id="societynamesection">
-        <div className="societyname">
-          <img src="/images/profileicon.svg" alt="Society image" />
+      <div id="alssocietysection">
+        <div className='alssocietyname'>
+          <img src="/images/societyicon.svg" alt="society name" />
           <label>Society Name</label>
         </div>
-
-        <div className="sideimage6">
-          <img src="/images/communitysideimg.svg" alt="dashboard sideimage" />
+        <div className='alssidelinks'>
+          <Link>Local Services</Link><br></br><br></br>
+          <Link to='/addlocalservice'>Add Local Services</Link>
         </div>
+        <div className='alssideimage'><img src="/images/societysideimg.svg" alt="society sideimage" /></div>
       </div>
-      <div className="addguestbackgroundimg">
-        <div className="Addguestdisplay3">
-          <label>Add Management Team</label>
+      <div className='alsbackgroundimg'>
+        <div className='alsdisplay'>
+          <label>Add Local Service</label>
         </div>
-        <Form className="formclass">
-          <div class="form-group5 row">
-            <label class="col-lg-2 col-form-label labelsize">Resident</label>
+        <Form className='formclass'>
+          <div class="form-group row">
+            <label class="col-lg-2 col-form-label labelsize">Vendor Name</label>
             <div class="col-lg-4">
-             <select className="form-control input-lg" id='resident_id'>
-                <option value={null} selected disabled>Select resident</option>
-                {/* {residents.map(item=>{
-                  return <option value={item.id}>{item.firstname} {item.lastname}</option>
-                })} */}
-             </select>
+              <input type="text" class="form-control input-lg" id='vendor_name' name="flatNo" placeholder="Vendor Name"></input>
             </div>
           </div>
           <div class="form-group row">
-            <label class="col-lg-2 col-form-label labelsize">
-              {" "}
-              Designation
-            </label>
+            <label class="col-lg-2 col-form-label labelsize">Vendor type</label>
             <div class="col-lg-4">
-              <input
-                type="text"
-                class="form-control input-lg"
-                name="Designation"
-                placeholder=""
-                id='management_title'
-              ></input>
+              <input type="text" class="form-control input-lg" id='venf=dor_type' name="flatNo" placeholder="Vendor type"></input>
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputentryno" class="col-sm-2 col-md-2 col-lg-2 col-form-label labelsize">Added By</label>
+            <div class="col-sm-4 col-md-4 col-lg-4">
+              <select class="form-control input-lg" id='added_by'>
+                <option value={null} disabled selected>Added By</option>
+              
+              </select>
+            </div>
+          </div>
+          <div class="form-group row">
+            <label class="col-lg-2 col-form-label labelsize">Status</label>
+            <div class="col-lg-4">
+              <select class="form-control input-lg" id="status" placeholder="Block">
+                <option value={null} disabled selected>Status</option>
+               
+              </select>
             </div>
           </div>
 
-          <div className="date row g-2">
-          <div class="col-md-3">
-              <label for="inputPassword4" class="form-label dateto">
-                From
-              </label>
-              <input
-                type="date"
-                class="form-control"
-                id="from"
-                name="From"
-              />
-            </div>
-            <div class="col-md-3">
-              <label for="inputEmail4" class="form-label dateto">
-                To
-              </label>
-              <input
-                type="date"
-                class="form-control"
-                id="to"
-                name="to"
-              />
-            </div>
-            
-          </div>
-
-          <Button type="submit"  className="btnAdd4">
-            Add
-          </Button>
+          <Button type="submit" className="btnAddV">Add Vendor</Button>
         </Form>
+
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Addlocalservice;
+export default Addlocalservice
+
