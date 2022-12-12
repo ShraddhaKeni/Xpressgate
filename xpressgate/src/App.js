b4ec33eb7bdf8aa62260c26d3020422a9a84f899
Jatin Mishra <jatin.mishra@axzoragroup.com>
2022-12-12 14:20:37 -0800

    adding tables screen of society due, utility pay, vendor pay

--- xpressgate/src/App.js
+++ xpressgate/src/App.js
@@ -77,6 +77,9 @@ import PackageList from './components/SocietyModule/PackageList';
 import AdminChangePass from './pages/admin/LoginScreens/AdminChangePass';
 import AdminReset from './pages/admin/LoginScreens/AdminReset';
 import Viewparking from './components/SocietyModule/Viewparking';
+import SocietyDuesTable from './components/SocietyModule/SocietyDuesTable';
+import VendorPaymentTable from './components/SocietyModule/VendorPaymentTable';
+import UtilityPaymentTable from './components/SocietyModule/UtilityPaymentTable';
 
 function App() {
 
@@ -194,6 +197,9 @@ function App() {
          
             <Route path="/packagelist" element={<PackageList />} exact></Route>
             <Route path="/paymenthistory" element={<SocietyPaymentHistory />} exact></Route>
+            <Route path="/societyduesrecord" element={<SocietyDuesTable />} exact></Route>
+            <Route path="/vendorpaymentrecord" element={<VendorPaymentTable />} exact></Route>
+            <Route path="/utilitypaymentrecord" element={<UtilityPaymentTable />} exact></Route>
         </Routes>
       </Router>
     </div>
