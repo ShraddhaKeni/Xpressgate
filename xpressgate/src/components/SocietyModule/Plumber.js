99aa35c19b7641c6c935fd6c868ceed3fe50fcba
Jatin Mishra <jatin.mishra@axzoragroup.com>
2022-12-14 16:56:27 -0800

    new changes

--- xpressgate/src/components/SocietyModule/Plumber.js
+++ xpressgate/src/components/SocietyModule/Plumber.js
@@ -113,8 +113,12 @@ const navigateTo =e=>{
         <div className="P_societyname">
           <img src="/images/societyicon.svg" alt="Society image" />
           <label>Society Name</label>
+        </div><br></br>
+        <div>
+        <a href="/localservices" className="LOcalSLINK">Local Services</a>
         </div>
         <div class="noticelist">
+        
         <div className="sidebar_classes">
           {services.map(item=>{
 
