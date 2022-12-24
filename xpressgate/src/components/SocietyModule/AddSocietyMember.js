377da80096ac8c4ee58730ada714ed7aae9db8fb
Jatin Mishra <jatin.mishra@axzoragroup.com>
2022-12-23 17:19:12 -0800

    new screen added

--- xpressgate/src/components/SocietyModule/AddSocietyMember.js
+++ xpressgate/src/components/SocietyModule/AddSocietyMember.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import "../SocietyModule/AddSocietyMember.css";
 import LogOut from './Utils/LogOut'
 import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
