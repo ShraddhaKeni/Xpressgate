c62e4c0d405af0cf022c6e3e46704da5c870573c
Gouse <gouse.mohiddin@axzora.com>
2022-12-26 12:47:49 +0530

    Update AddSocietyMember.js

--- xpressgate/src/components/SocietyModule/AddSocietyMember.js
+++ xpressgate/src/components/SocietyModule/AddSocietyMember.js
@@ -1,11 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useNavigate } from "react";
 import "../SocietyModule/AddSocietyMember.css";
 import LogOut from './Utils/LogOut'
 import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
+import axios from "axios";
 
 
 const AddSocietyMember = () => {
+  const [member, setMember] = useState({})
+  const [file, setProfilePicFile] = useState()
+  const navigate = useNavigate;
+  const [communities, setCommunity] = useState([])
+  useEffect(() => {
+    getCommunities()
+  }, [])
+
+  const getCommunities = async () => {
+    try {
+      const { data } = await axios.get(`${window.env_var}api/community/get`)
+      setCommunity(data.data.community);
+    } catch (error) {
+
+    }
+  }
+
+
+  const handleAddSocietyMember = async (e) => {
+    e.preventDefault();
+    alert(JSON.stringify(member))
+    // Pass member to add Society member api - POST
+    // What to do with the profile pic?
+    try {
+      const formData = new FormData();
+      formData.append('profile_pic', file)
+      for (var key in member) {
+        formData.append(key, member[key]);
+      }
+      const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }
+      const { data } = await axios.post(`${window.env_var}api/community/addResident`, formData, config);
+      navigate(-1);
+    } catch (error) {
+      alert('Unable to add Society Member');
+    }
+  }
 
   return (
     <div className="addguestcontainer4">
@@ -26,7 +67,7 @@ const AddSocietyMember = () => {
           <img src="/images/societyicon.svg" alt="Society image" />
           <label>Society Name</label>
         </div>
-       
+
         <div className="ASocMSideIMG">
           <img src="/images/communitysideimg.svg" alt="dashboard sideimage" />
         </div>
@@ -35,69 +76,97 @@ const AddSocietyMember = () => {
         <div className="AMM_display">
           <label>Add Society Member</label>
         </div>
-        <Form className="formclass">
+        <Form className="formclass" onSubmit={handleAddSocietyMember}>
           <div class="form-group row">
-                <label class="col-lg-2 col-form-label ADN_label">Community id</label>
-                <div class="col-lg-4">
-                  <select type="text" class="form-control input-lg SideB" id='community_id' name="community id" >
-                    <option  disabled value={null} selected></option>
-                      
-                  </select>
-                </div>
-              </div>
-              <div class="form-group row">
-                <label class="col-lg-2 col-form-label ADN_label">First Name</label>
-                <div class="col-lg-4">
-                  <input type="text" class="form-control input-lg SideB" id='First_name'  name="First name" placeholder="First Name"> 
-                  </input>
-                  
-                </div>
-              </div>
-              <div class="form-group row">
-                <label class="col-lg-2 col-form-label ADN_label">Last Name</label>
-                <div class="col-lg-4">
-                  <input type="text" class="form-control input-lg SideB" id='last_name'  name="Last name" placeholder="Last Name"> 
-                  </input>
-                  
-                </div>
-              </div>
-              <div class="form-group row">
-                <label class="col-lg-2 col-form-label ADN_label">Gender</label>
-                <div class="col-lg-4">
-                  <select type="text" class="form-control input-lg SideB" id='gender' name="Gender" >
-                    <option  disabled value={null} selected>Male</option>
-                      
-                  </select>
-                </div>
-              </div>
-              <div class="form-group row">
-                <label class="col-lg-2 col-form-label ADN_label">User Name</label>
-                <div class="col-lg-4">
-                  <input type="text" class="form-control input-lg SideB" id='user_name' name="User Name" />
-                </div>
-              </div>
-              <div class="form-group row">
-                <label class="col-lg-2 col-form-label ADN_label">Mobile No</label>
-                <div class="col-lg-4">
-                  <input type="number" class="form-control input-lg SideB" id='mobile_no' name="Mobile No" />
-                </div>
-              </div>
-              <div class="form-group row">
-                <label class="col-lg-2 col-form-label ADN_label">Email</label>
-                <div class="col-lg-4">
-                  <input type="email" class="form-control input-lg SideB" id='email' name="Email" />
-                </div>
-              </div>
-              <div class="form-group row">
-                <label class="col-lg-2 col-form-label ADN_label">Profile picture</label>
-                <div class="col-lg-4">
-                  <input type="file" class="form-control input-lg SideB" id='picture' name="Profile pic"> 
-                  </input>
-                </div>
-              </div>
-              <button type="submit" className="ASMBtn_Add">
-                  Add Member
-              </button>
+            <label class="col-lg-2 col-form-label ADN_label">Community</label>
+            <div class="col-lg-4">
+              <select type="text" class="form-control input-lg SideB" id='community_id' name="community_id"
+                onChange={(e) => { setMember({ ...member, community_id: e.target.value }); console.log(member) }}
+              >
+                <option value={null} selected>Select Community</option>
+                {communities.length && communities.map((community) => {
+                  return <option value={community._id} >{community.name}</option>
+                })}
+
+              </select>
+            </div>
+          </div>
+          <div class="form-group row">
+            <label class="col-lg-2 col-form-label ADN_label">First Name</label>
+            <div class="col-lg-4">
+              <input type="text" class="form-control input-lg SideB" id='First_name' name="firstname" placeholder="First Name"
+                onChange={(e) => { setMember({ ...member, firstname: e.target.value }); console.log(member) }}
+              >
+              </input>
+
+            </div>
+          </div>
+          <div class="form-group row">
+            <label class="col-lg-2 col-form-label ADN_label">Last Name</label>
+            <div class="col-lg-4">
+              <input type="text" class="form-control input-lg SideB" id='last_name' name="lastname" placeholder="Last Name"
+                onChange={(e) => { setMember({ ...member, lastname: e.target.value }); console.log(member) }}
+
+              >
+              </input>
+
+            </div>
+          </div>
+          <div class="form-group row">
+            <label class="col-lg-2 col-form-label ADN_label">Gender</label>
+            <div class="col-lg-4">
+              <select type="text" class="form-control input-lg SideB" id='gender' name="Gender"
+                onChange={(e) => { setMember({ ...member, gender: e.target.value }); console.log(member) }}
+              >
+                <option disabled value={'1'} selected>Male</option>
+                <option value={'2'}>Female</option>
+
+              </select>
+            </div>
+          </div>
+          <div class="form-group row">
+            <label class="col-lg-2 col-form-label ADN_label">User Name</label>
+            <div class="col-lg-4">
+              <input type="text" class="form-control input-lg SideB" id='username' name="username"
+                onChange={(e) => { setMember({ ...member, username: e.target.value }); console.log(member) }}
+
+              />
+            </div>
+          </div>
+          <div class="form-group row">
+            <label class="col-lg-2 col-form-label ADN_label">Mobile No</label>
+            <div class="col-lg-4">
+              <input type="number" class="form-control input-lg SideB" id='mobile_no' name="Mobile No"
+                onChange={(e) => { setMember({ ...member, mobileno: e.target.value }); console.log(member) }}
+
+              />
+            </div>
+          </div>
+          <div class="form-group row">
+            <label class="col-lg-2 col-form-label ADN_label">Email</label>
+            <div class="col-lg-4">
+              <input type="email" class="form-control input-lg SideB" id='email' name="Email"
+                onChange={(e) => { setMember({ ...member, email: e.target.value }); console.log(member) }}
+
+              />
+            </div>
+          </div>
+
+          <div class="form-group row">
+            <label class="col-lg-2 col-form-label ADN_label">Profile picture</label>
+            <div class="col-lg-4">
+              <input type="file" class="form-control input-lg SideB" id='picture' name="profile_pic"
+                onChange={(e) => {
+                  let file = e.target.files[0];
+                  setProfilePicFile(file)
+                }}
+              >
+              </input>
+            </div>
+          </div>
+          <button type="submit" className="ASMBtn_Add">
+            Add Member
+          </button>
         </Form>
       </div>
     </div>
