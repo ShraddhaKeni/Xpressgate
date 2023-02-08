e2bf1dfd0a00546d7d6388055c176eb34411dbfc
Gouse <gouse.mohiddin@axzora.com>
2023-02-08 10:07:52 +0530

    fixes to open issues

--- xpressgate/src/components/GuardModule/Addinout.js
+++ xpressgate/src/components/GuardModule/Addinout.js
@@ -19,7 +19,7 @@ const Addinout = () => {
   const [vendor, setVendor] = useState([])
   const [dailyhelp, setDailyhelp] = useState([])
   const [residents, setResidents] = useState([])
-  const [isError,setError] = useState(false)
+  const [isError, setError] = useState(false)
 
   // let blockid = document.getElementById('item').value
   const visitortype = useRef([])
@@ -40,7 +40,7 @@ const Addinout = () => {
           localStorage.clear();
           window.location.href = '/guardLogin'
         })
-        setLoading(false);
+      setLoading(false);
     } else {
       window.location.href = '/'
     }
@@ -75,9 +75,9 @@ const Addinout = () => {
   const getResidents = async (e) => {
     try {
       const config = {
-        headers:{'x-access-token':localStorage.getItem('accesstoken')},
+        headers: { 'x-access-token': localStorage.getItem('accesstoken') },
       }
-      const { data } = await axios.get(`${window.env_var}api/resident/getResidentByFlatId/${e.target.value}`,config)
+      const { data } = await axios.get(`${window.env_var}api/resident/getResidentByFlatId/${e.target.value}`, config)
       //setResidents(data.data.list)
       setError(false)
     } catch (error) {
@@ -101,7 +101,7 @@ const Addinout = () => {
         setDailyhelp(data.data.dailyhelp)
         setError(false)
       }
-      
+
     } catch (error) {
       setError(true)
     }
@@ -112,7 +112,7 @@ const Addinout = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-     
+
       let date = new Date(document.getElementById('date').value + 'T' + document.getElementById('intime').value + ':00').toISOString()
       const sendData = {
         firstname: document.getElementById('name').value,
@@ -133,7 +133,7 @@ const Addinout = () => {
       console.log(data)
       setToast({ show: true, type: "success", message: "Added Successfully" })
       setTimeout(() => {
-        window.location.href='/inoutbook'
+        window.location.href = '/inoutbook'
       }, 1500);
       // window.location.href = '/inoutbook'
     } catch (error) {
@@ -141,8 +141,8 @@ const Addinout = () => {
     }
   }
 
-  if(isError)
-    return <ErrorScreen/>
+  if (isError)
+    return <ErrorScreen />
 
   return (
     <div className="aiocontainer">
@@ -159,25 +159,25 @@ const Addinout = () => {
         </div>
       </div>
       <div className='aiobackgroundimg'>
-      <ToastMessage show={toast.show} message={toast.message} type={toast.type} handleClose={() => { setToast({ show: false }) }} />
+        <ToastMessage show={toast.show} message={toast.message} type={toast.type} handleClose={() => { setToast({ show: false }) }} />
         <div className='aiodisplay'>
           <label>Add In Out</label>
         </div>
         {/* <Loader loading={loading}> */}
 
-        <Form className='AIOformclass'>
-        
+        <Form className='AIOformclass' onSubmit={(e) => { handleSubmit(e) }} >
+
           <div className="form-group row">
-         
+
             <label className="col-lg-2 col-form-label ADN_label">Name</label>
             <div className="col-lg-4">
-              <input type="text" className="form-control input-lg AIOBorder" id='name' name="addinoutname" placeholder="Name" value={details.name}></input>
+              <input required type="text" className="form-control input-lg AIOBorder" id='name' name="addinoutname" placeholder="Name" value={details.name}></input>
             </div>
           </div>
           <div className="form-group row">
             <label for="inputentryno" className="col-sm-2 col-md-2 col-lg-2 col-form-label ADN_label">Visitor Type</label>
             <div className="col-sm-4 col-md-4 col-lg-4">
-              <select class="form-control input-lg AIOBorder" ref={visitortype} name="visitortype" placeholder="Visitor Type" id="visitortype" onChange={(e) => getDetails(e)}>
+              <select required class="form-control input-lg AIOBorder" ref={visitortype} name="visitortype" placeholder="Visitor Type" id="visitortype" onChange={(e) => getDetails(e)}>
                 <option value={1}>Guest</option>
                 <option value={2}>Vendor</option>
                 <option value={3}>Daily Helper</option>
@@ -187,7 +187,7 @@ const Addinout = () => {
           <div className="form-group row" id='fullvendor'>
             <label for="inputentryno" className="col-sm-2 col-md-2 col-lg-2 col-form-label ADN_label">Select Vendor</label>
             <div className="col-sm-4 col-md-4 col-lg-4">
-              <select className="form-control input-lg AIOBorder" id="vendor" placeholder="Vendor">
+              <select required className="form-control input-lg AIOBorder" id="vendor" placeholder="Vendor">
                 <option value="" selected disabled>Select Vendor</option>
                 {vendor.map(item => {
                   return <option value={item._id}>{item.vendorName}</option>
@@ -198,7 +198,7 @@ const Addinout = () => {
           <div className="form-group row" id='fulldailyhelp'>
             <label for="inputentryno" className="col-sm-2 col-md-2 col-lg-2 col-form-label ADN_label">Select Daily Helper</label>
             <div className="col-sm-4 col-md-4 col-lg-4">
-              <select className="form-control input-lg AIOBorder" id="dailyhelp" placeholder="Daily Helper">
+              <select required className="form-control input-lg AIOBorder" id="dailyhelp" placeholder="Daily Helper">
                 <option value="" selected disabled>Select Daily Helper</option>
                 {dailyhelp.map(item => {
                   return <option value={item.id}>{item.serviceType}</option>
@@ -209,7 +209,7 @@ const Addinout = () => {
           <div className="form-group row">
             <label for="inputentryno" className="col-sm-2 col-md-2 col-lg-2 col-form-label ADN_label">Block</label>
             <div className="col-sm-4 col-md-4 col-lg-4">
-              <select className="form-control input-lg AIOBorder" id='block' onChange={(e) => getFlats(e)}>
+              <select required className="form-control input-lg AIOBorder" id='block' onChange={(e) => getFlats(e)}>
                 <option value={null} disabled selected>Block</option>
                 {block.map(item => {
                   return (
@@ -223,7 +223,7 @@ const Addinout = () => {
           <div className="form-group row">
             <label className="col-lg-2 col-form-label ADN_label">Flat No.</label>
             <div className="col-lg-4">
-              <select className="form-control input-lg AIOBorder" id="flatno" placeholder="Flat No." onChange={(e) => getResidents(e)}>
+              <select required className="form-control input-lg AIOBorder" id="flatno" placeholder="Flat No." onChange={(e) => getResidents(e)}>
                 <option value="" selected disabled>Select Flat</option>
                 {flatno.map(item => {
                   return <option value={item._id}>{item.flat_number}</option>
@@ -234,7 +234,7 @@ const Addinout = () => {
           <div className="form-group row">
             <label className="col-lg-2 col-form-label ADN_label">Resident</label>
             <div className="col-lg-4">
-              <select className="form-control input-lg AIOBorder" id="resident" placeholder="Resident">
+              <select required className="form-control input-lg AIOBorder" id="resident" placeholder="Resident">
                 <option value="" selected disabled>Select Resident</option>
                 {flatno.map(item => {
                   return <option value={item._id}>{item.firstname}{item.lastname}</option>
@@ -245,33 +245,33 @@ const Addinout = () => {
           <div className="form-group row">
             <label className="col-lg-2 col-form-label ADN_label">Contact No.</label>
             <div className="col-lg-4">
-              <input type="text" className="form-control input-lg AIOBorder" id='contact_no' name="ContactNo" placeholder="Contact No." value={details.contact_no} maxLength="10"></input>
+              <input required type="text" className="form-control input-lg AIOBorder" id='contact_no' name="ContactNo" placeholder="Contact No." value={details.contact_no} maxLength="10"></input>
             </div>
           </div>
           <div className="form-group row">
             <label className="col-lg-2 col-form-label ADN_label">Date</label>
             <div className="col-lg-4">
-              <input type="date" className="form-control input-lg AIOBorder" id='date' name="date" placeholder="Date" value={details.date}></input>
+              <input required type="date" className="form-control input-lg AIOBorder" id='date' name="date" placeholder="Date" value={details.date}></input>
             </div>
           </div>
           <div className="form-group row">
             <label className="col-lg-2 col-form-label ADN_label">In Time</label>
             <div className="col-lg-4">
-              <input type="time" className="form-control input-lg AIOBorder" id='intime' name="intime" placeholder=" In Time" value={details.intime} ></input>
+              <input required type="time" className="form-control input-lg AIOBorder" id='intime' name="intime" placeholder=" In Time" value={details.intime} ></input>
             </div>
           </div>
           <div className="form-group row hidden">
             <label className="col-lg-2 col-form-label ADN_label">Status</label>
             <div className="col-lg-4">
-              <select class="form-control input-lg AIOBorder" id='status' placeholder="Status" disabled>
+              <select required class="form-control input-lg AIOBorder" id='status' placeholder="Status" disabled>
                 <option value={1} selected>In</option>
                 <option value={2}>Out</option>
               </select>
             </div>
           </div>
 
-          <button type="submit" onClick={(e) => { handleSubmit(e) }} className="btnInOut" on>Add In Out</button>
-         
+          <button type="submit" className="btnInOut" on>Add In Out</button>
+
         </Form>
         {/* </Loader> */}
       </div>
