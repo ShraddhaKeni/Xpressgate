454157fff6f34bf323bc75652912ad0344a5e1d9
Jatin Mishra <jatin.mishra@axzoragroup.com>
2023-03-03 16:39:55 -0800

    added new scanner code in Guard's DashBoard

--- xpressgate/src/components/GuardModule/Dashboard.js
+++ xpressgate/src/components/GuardModule/Dashboard.js
@@ -13,6 +13,8 @@ import { Loader } from "../Loader";
 import GuardMobileSidebar from '../GuardMobileSidebar';
 import { QrReader } from 'react-qr-reader';
 
+
+
 // import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
 const Dashboard = () => {
@@ -20,15 +22,16 @@ const Dashboard = () => {
   const [entryData, setEntryData] = useState({})
   const [message, setMessage] = useState({})
   const [menu, setMenuOpen] = useState(false)
-  // const [qrdata, setData] = React.useState("Not Found");
-  // const [torchOn, setTorchOn] = React.useState(false);
-  const [selected, setSelected] = useState("environment");
-  const [startScan, setStartScan] = useState(false);
-  const [loadingScan, setLoadingScan] = useState(false);
-  const [data, setData] = useState("");
+ 
+  const [result, setResult] = useState('');
+  // const [selected, setSelected] = useState("environment");
+  // const [startScan, setStartScan] = useState(false);
+  // const [loadingScan, setLoadingScan] = useState(false);
+  // const [qrdata, setData] = useState("");
 
   useEffect(() => {
     if (checkGuard()) {
+      
       const config = {
         headers: {
           'x-access-token': localStorage.getItem('accesstoken')
@@ -99,22 +102,31 @@ const Dashboard = () => {
     return d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate()
 
   }
+  
+  const handleError = error => {
+     console.error(error);
+     };
+     const handleScan = data => {
+    if (data) {
+       setResult(data);
+       }
+      };
 // ScannerCode
 
-  const handleScan = async (scanData) => {
-    setLoadingScan(true);
-    console.log(`loaded data data`, scanData);
-    if (scanData && scanData !== "") {
-      console.log(`loaded >>>`, scanData);
-      setData(scanData);
-      setStartScan(false);
-      setLoadingScan(false);
-      // setPrecScan(scanData);
-    }
-  };
-  const handleError = (err) => {
-    console.error(err);
-  };
+  // const handleScan = async (scanData) => {
+  //   setLoadingScan(true);
+  //   console.log(`loaded data data`, scanData);
+  //   if (scanData && scanData !== "") {
+  //     console.log(`loaded >>>`, scanData);
+  //     setData(scanData);
+  //     setStartScan(false);
+  //     setLoadingScan(false);
+  //     // setPrecScan(scanData);
+  //   }
+  // };
+  // const handleError = (err) => {
+  //   console.error(err);
+  // };
 
 
   return (
@@ -135,19 +147,7 @@ const Dashboard = () => {
         <div className='dashboardbackgroundimg'>
           <Loader loading={loading}>
 
-          {/* <BarcodeScannerComponent
-        width={500}
-        height={500}
-        torch={torchOn}
-        onUpdate={(err, result) => {
-          if (result) setData(result.text);
-          else setData("Not Found");
-        }}
-      />
-      <p>{qrdata}</p>
-      <button onClick={() => setTorchOn(!torchOn)}>
-        Switch Torch {torchOn ? "Off" : "On"}
-      </button> */}
+      
  
             <div id="cardsection">
               <div className='enterpasscodesearch'>
@@ -164,8 +164,20 @@ const Dashboard = () => {
                 <img src="/images/searchicon.svg" className='search_icon' onClick={() => { checkInputs() }} alt="search" />
               </div>
 
-              {/* Scanner Code */}
+ {/* Scanner Code 1*/}
               <div className='ScannerContainer'>
+              <QrReader
+               delay={300}
+               onError={handleError}
+               onScan={handleScan}
+               style={{ width: '100%' }}
+       /><p>{result}</p>
+      </div>
+
+
+{/* SCANNER CODE 2 */}
+
+              {/* <div className='ScannerContainer'>
               <button
               className='ScannerButn'
         onClick={() => {
@@ -181,7 +193,7 @@ const Dashboard = () => {
             <option value={"environment"}>Back Camera</option>
             <option value={"user"}>Front Camera</option>
           </select>
-          
+        
           <QrReader
          
             facingMode={selected}
@@ -196,8 +208,8 @@ const Dashboard = () => {
         </>
       )}
       {loadingScan && <p>Loading</p>}
-      {data !== "" && <p>{data}</p>}
-              </div>
+      {qrdata !== "" && <p>{qrdata}</p>}
+              </div> */}
               <div className="row row-cols-1 row-cols-md-3 g-4 FullCardsCss allcards">
                 <div className="col">
                   <div className="DashBoardCard">
