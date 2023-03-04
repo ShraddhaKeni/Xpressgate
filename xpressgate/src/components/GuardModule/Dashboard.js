167ee46528e5bfbf79b94ae24c7e0e3941e8467f
Jatin Mishra <jatin.mishra@axzoragroup.com>
2023-03-03 18:51:30 -0800

    latest change

--- xpressgate/src/components/GuardModule/Dashboard.js
+++ xpressgate/src/components/GuardModule/Dashboard.js
@@ -11,9 +11,6 @@ import { passcodeValidation } from '../auth/validation';
 import GuardHeader from './Utils/GuardHeader';
 import { Loader } from "../Loader";
 import GuardMobileSidebar from '../GuardMobileSidebar';
-import { QrReader } from 'react-qr-reader';
-
-
 
 // import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
@@ -22,16 +19,10 @@ const Dashboard = () => {
   const [entryData, setEntryData] = useState({})
   const [message, setMessage] = useState({})
   const [menu, setMenuOpen] = useState(false)
- 
-  const [result, setResult] = useState('');
-  // const [selected, setSelected] = useState("environment");
-  // const [startScan, setStartScan] = useState(false);
-  // const [loadingScan, setLoadingScan] = useState(false);
-  // const [qrdata, setData] = useState("");
-
+  // const [qrdata, setData] = React.useState("Not Found");
+  // const [torchOn, setTorchOn] = React.useState(false);
   useEffect(() => {
     if (checkGuard()) {
-      
       const config = {
         headers: {
           'x-access-token': localStorage.getItem('accesstoken')
@@ -102,31 +93,8 @@ const Dashboard = () => {
     return d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate()
 
   }
-  
-  const handleError = error => {
-     console.error(error);
-     };
-     const handleScan = data => {
-    if (data) {
-       setResult(data);
-       }
-      };
-// ScannerCode
 
-  // const handleScan = async (scanData) => {
-  //   setLoadingScan(true);
-  //   console.log(`loaded data data`, scanData);
-  //   if (scanData && scanData !== "") {
-  //     console.log(`loaded >>>`, scanData);
-  //     setData(scanData);
-  //     setStartScan(false);
-  //     setLoadingScan(false);
-  //     // setPrecScan(scanData);
-  //   }
-  // };
-  // const handleError = (err) => {
-  //   console.error(err);
-  // };
+
 
 
   return (
@@ -147,7 +115,19 @@ const Dashboard = () => {
         <div className='dashboardbackgroundimg'>
           <Loader loading={loading}>
 
-      
+          {/* <BarcodeScannerComponent
+        width={500}
+        height={500}
+        torch={torchOn}
+        onUpdate={(err, result) => {
+          if (result) setData(result.text);
+          else setData("Not Found");
+        }}
+      />
+      <p>{qrdata}</p>
+      <button onClick={() => setTorchOn(!torchOn)}>
+        Switch Torch {torchOn ? "Off" : "On"}
+      </button> */}
  
             <div id="cardsection">
               <div className='enterpasscodesearch'>
@@ -163,53 +143,6 @@ const Dashboard = () => {
 
                 <img src="/images/searchicon.svg" className='search_icon' onClick={() => { checkInputs() }} alt="search" />
               </div>
-
- {/* Scanner Code 1*/}
-              <div className='ScannerContainer'>
-              <QrReader
-               delay={300}
-               onError={handleError}
-               onScan={handleScan}
-               style={{ width: '100%' }}
-       /><p>{result}</p>
-      </div>
-
-
-{/* SCANNER CODE 2 */}
-
-              {/* <div className='ScannerContainer'>
-              <button
-              className='ScannerButn'
-        onClick={() => {
-          setStartScan(!startScan);
-        }}
-      >
-        {startScan ? "Stop Scan" : "Start Scan"}
-      </button>
-      {startScan && (
-        <>
-        <br/>
-      <select onChange={(e) => setSelected(e.target.value)} className="SselectButn">
-            <option value={"environment"}>Back Camera</option>
-            <option value={"user"}>Front Camera</option>
-          </select>
-        
-          <QrReader
-         
-            facingMode={selected}
-            delay={1000}
-            onError={handleError}
-            onScan={handleScan}
-            // chooseDeviceId={()=>selected}
-            style={{ width: "300px" }}
-            className='VideoContainer'
-          />
-        
-        </>
-      )}
-      {loadingScan && <p>Loading</p>}
-      {qrdata !== "" && <p>{qrdata}</p>}
-              </div> */}
               <div className="row row-cols-1 row-cols-md-3 g-4 FullCardsCss allcards">
                 <div className="col">
                   <div className="DashBoardCard">
