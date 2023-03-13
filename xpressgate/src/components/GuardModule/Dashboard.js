d88eab1c7f11fb0442bee46acd7e83acb00596cf
Keshav Methar <keshav.methar@axzora.com>
2023-03-13 11:10:46 +0530

    Fixed Scan QR

--- xpressgate/src/components/GuardModule/Dashboard.js
+++ xpressgate/src/components/GuardModule/Dashboard.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Dashboard.css';
-import { Button } from 'react-bootstrap';
 import axios from 'axios';
-import { Link, Navigate } from 'react-router-dom'
 import LogOut from './Utils/LogOut';
 import Frequentvisitor from './Frequentvisitor';
 import Dailyservicepasscode from './Dailyservicepasscode';
@@ -11,64 +9,55 @@ import { passcodeValidation } from '../auth/validation';
 import GuardHeader from './Utils/GuardHeader';
 import { Loader } from "../Loader";
 import GuardMobileSidebar from '../GuardMobileSidebar';
-import { QrReader } from 'react-qr-reader';
-
-
-
-// import BarcodeScannerComponent from "react-qr-barcode-scanner";
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
 const Dashboard = () => {
-  const [loading, setLoading] = useState(true)
-  const [entryData, setEntryData] = useState({})
-  const [message, setMessage] = useState({})
-  const [menu, setMenuOpen] = useState(false)
- 
+  const [loading, setLoading] = useState(true);
+  const [entryData, setEntryData] = useState({});
+  const [message, setMessage] = useState({});
+  const [menu, setMenuOpen] = useState(false);
   const [result, setResult] = useState('');
-  // const [selected, setSelected] = useState("environment");
-  // const [startScan, setStartScan] = useState(false);
-  // const [loadingScan, setLoadingScan] = useState(false);
-  // const [qrdata, setData] = useState("");
-
+  const [data, setData] = useState("Not Found");
+  const [torchOn, setTorchOn] = useState(false);
+ 
   useEffect(() => {
     if (checkGuard()) {
-      
       const config = {
         headers: {
           'x-access-token': localStorage.getItem('accesstoken')
         }
       }
       axios.get(`${window.env_var}api/guard/checkLogin`, config)
-        .then(({ data }) => {
+      .then(({ data }) => {
 
-        })
-        .catch(err => {
-          localStorage.clear();
-          window.location.href = '/guardLogin'
-        })
+      })
+      .catch(err => {
+        localStorage.clear();
+        window.location.href = '/guardLogin'
+      })
       setLoading(false);
     } else {
       window.location.href = '/'
     }
   }, [])
-  const checkInputs = async () => {
-    let a = document.getElementById('1').value
-    let b = document.getElementById('2').value
-    let c = document.getElementById('3').value
-    let d = document.getElementById('4').value
-    let e = document.getElementById('5').value
-    let f = document.getElementById('6').value
 
-    let code = parseInt(a + b + c + d + e + f)
+  const checkInputs = async () => {
+    let a = document.getElementById('1').value;
+    let b = document.getElementById('2').value;
+    let c = document.getElementById('3').value;
+    let d = document.getElementById('4').value;
+    let e = document.getElementById('5').value;
+    let f = document.getElementById('6').value;
+
+    let code = parseInt(a + b + c + d + e + f);
 
     try {
-
       if (await passcodeValidation(code)) {
         const codeData = {
           code: code,
-          community_id: "632970d054edb049bcd0f0b4"
+          community_id: localStorage.getItem('community_id')
         }
         let { data } = await axios.post(`${window.env_var}api/inoutentires/getdata`, codeData)
-        console.log(data.data)
         if (data.data.bookingdetails.status === false /* || dateTimeFormat(data.data.bookingdetails.date)!=dateTimeFormat(Date.now()) */) {
           alert('Expired Entry Code.')
           return
@@ -77,11 +66,33 @@ const Dashboard = () => {
           setEntryData(data.data.bookingdetails)
           setMessage(data.message)
         }
-
       } else {
         alert('Enter valid passcode')
       }
+    } catch (error) {
+      console.log('Please check again');
+    }
+  }
 
+  const scanresult = async (code) => {
+    try {
+      if (await passcodeValidation(code)) {
+        const codeData = {
+          code: code,
+          community_id: localStorage.getItem('community_id')
+        }
+        let { data } = await axios.post(`${window.env_var}api/inoutentires/getdata`, codeData)
+        if (data.data.bookingdetails.status === false /* || dateTimeFormat(data.data.bookingdetails.date)!=dateTimeFormat(Date.now()) */) {
+          alert('Expired Entry Code.');
+          return
+        }
+        else {
+          setEntryData(data.data.bookingdetails)
+          setMessage(data.message)
+        }
+      } else {
+        alert('Enter valid passcode')
+      }
     } catch (error) {
       console.log('Please check again');
     }
@@ -100,34 +111,7 @@ const Dashboard = () => {
   const dateTimeFormat = (date) => {
     var d = new Date(date)
     return d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate()
-
   }
-  
-  const handleError = error => {
-     console.error(error);
-     };
-     const handleScan = data => {
-    if (data) {
-       setResult(data);
-       }
-      };
-// ScannerCode
-
-  // const handleScan = async (scanData) => {
-  //   setLoadingScan(true);
-  //   console.log(`loaded data data`, scanData);
-  //   if (scanData && scanData !== "") {
-  //     console.log(`loaded >>>`, scanData);
-  //     setData(scanData);
-  //     setStartScan(false);
-  //     setLoadingScan(false);
-  //     // setPrecScan(scanData);
-  //   }
-  // };
-  // const handleError = (err) => {
-  //   console.error(err);
-  // };
-
 
   return (
     <>
@@ -146,9 +130,6 @@ const Dashboard = () => {
         </div>
         <div className='dashboardbackgroundimg'>
           <Loader loading={loading}>
-
-      
- 
             <div id="cardsection">
               <div className='enterpasscodesearch'>
                 <label>ENTER PASSCODE</label>
@@ -160,56 +141,25 @@ const Dashboard = () => {
                   <input type='text' className='dashboard_passcode' onKeyUp={e => { shiftFocus(e) }} maxLength="1" id='5'></input>
                   <input type='text' className='dashboard_passcode' onKeyUp={e => { shiftFocus(e) }} maxLength="1" id='6'></input>
                 </div>
-
                 <img src="/images/searchicon.svg" className='search_icon' onClick={() => { checkInputs() }} alt="search" />
               </div>
 
- {/* Scanner Code 1*/}
               <div className='ScannerContainer'>
-              <QrReader
-               delay={300}
-               onError={handleError}
-               onScan={handleScan}
-               style={{ width: '100%' }}
-       /><p>{result}</p>
-      </div>
+                <BarcodeScannerComponent
+                  width={500}
+                  height={500}
+                  torch={torchOn}
+                  onUpdate={(err, result) => {
+                    if (result) scanresult(result.text);
+                    else setData("Not Found");
+                  }}
+                />
+                {/* <p>{data}</p> */}
+                <button onClick={() => setTorchOn(!torchOn)}>
+                  Switch Torch {torchOn ? "Off" : "On"}
+                </button>
+              </div>
 
-
-{/* SCANNER CODE 2 */}
-
-              {/* <div className='ScannerContainer'>
-              <button
-              className='ScannerButn'
-        onClick={() => {
-          setStartScan(!startScan);
-        }}
-      >
-        {startScan ? "Stop Scan" : "Start Scan"}
-      </button>
-      {startScan && (
-        <>
-        <br/>
-      <select onChange={(e) => setSelected(e.target.value)} className="SselectButn">
-            <option value={"environment"}>Back Camera</option>
-            <option value={"user"}>Front Camera</option>
-          </select>
-        
-          <QrReader
-         
-            facingMode={selected}
-            delay={1000}
-            onError={handleError}
-            onScan={handleScan}
-            // chooseDeviceId={()=>selected}
-            style={{ width: "300px" }}
-            className='VideoContainer'
-          />
-        
-        </>
-      )}
-      {loadingScan && <p>Loading</p>}
-      {qrdata !== "" && <p>{qrdata}</p>}
-              </div> */}
               <div className="row row-cols-1 row-cols-md-3 g-4 FullCardsCss allcards">
                 <div className="col">
                   <div className="DashBoardCard">
@@ -241,7 +191,6 @@ const Dashboard = () => {
           </Loader>
         </div>
       </div>}
-
       <GuardMobileSidebar open={menu} onHide={() => setMenuOpen(false)} />
 
     </>
