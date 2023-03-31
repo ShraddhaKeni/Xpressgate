ffb3feda6b2b94b9a9db3d154ea0a8f4ddafd268
Keshav Methar <keshav.methar@axzora.com>
2023-03-31 10:12:00 +0530

    Fixed add commission

--- xpressgate/src/pages/admin/CommissionManagement/AddCommission.js
+++ xpressgate/src/pages/admin/CommissionManagement/AddCommission.js
@@ -12,114 +12,85 @@ import { goBackInOneSec, TOAST } from '../../../common/utils';
 import { Email } from '@mui/icons-material';
 
 const AddCommission = () => {
-
-    const [commission, setCommission] = useState({
-        program_id: '',
-       
-            amount: '',
-        commission: '',
+  const [commission, setCommission] = useState({
+    program_id: '',
+    amount: '',
+    commission: '',
     payment_status: false,
-            status: true
+    status: true    
+  })
+  const [toast, setToast] = useState({ show: false })
+  const [allprograms, setAllPrograms] = useState([]);
+  const location = useLocation();
+  const [isError, setError] = useState(false)
+  const navigate = useNavigate()
+  useEffect(() => {
+    getAllProgram()
+  }, [])
 
-        
-    })
-    const [toast, setToast] = useState({ show: false })
-    const [allprograms, setAllPrograms] = useState([]);
-    const location = useLocation();
-    const [isError, setError] = useState(false)
-    const navigate = useNavigate()
-    useEffect(() => {
-        getAllProgram()
-    }, [])
-    const getAllProgram = async () => {
-        try {
-        
-          const { data } = await axios.get(`${window.env_var}api/partner/programs`)
-          console.log(data)
-          setAllPrograms(data.data)
-        
-        //   document.getElementById('programname').value = location.state.id;
-        
-          
-          setError(false)
-        } catch (error) {
-          setError(true)
-        }
+  const getAllProgram = async () => {
+    try {  
+      const { data } = await axios.get(`${window.env_var}api/partner/programs`)
+      setAllPrograms(data.data)
+      setError(false)
+    } catch (error) {
+      setError(true)
+    }
+  }
+
+  const getProgramDetails = async (e) => {
+    //console.log(e.target.selectedOptions[0].id);
+    setCommission({ ...commission, amount: e.target.selectedOptions[0].id,program_id: e.target.value }) 
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const { data } = await axios.post(`${window.env_var}api/commission/add`, commission)
+      if (data.status_code == 200) {
+        setToast(TOAST.SUCCESS(data?.message));
+        goBackInOneSec(navigate)
+      } else {
+        setToast(TOAST.ERROR(data?.message));
       }
-    const getProgramDetails = async (e) => { 
-        setCommission({ ...commission, program_id: e.target.value }) 
-        console.log(e.target.value )
-        const { data } = await axios.get(`${window.env_var}api/partner/programs/${e.target.value}`)
-        console.log(data.data.fee)
-       
-  
-
-         setCommission({ ...commission, amount: data.data.fee }) 
     }
-
-
-
-    const handleSubmit = async (e) => {
-        console.log(commission)
-        e.preventDefault()
-        try {
-
-         
-                const { data } = await axios.post(`${window.env_var}api/commission/add`, commission)
-                if (data.status_code == 200) {
-                    setToast(TOAST.SUCCESS(data?.message));
-                    goBackInOneSec(navigate)
-                } else {
-                    setToast(TOAST.ERROR(data?.message));
-                }
-            }
-          
-
-         catch (error) {
-            console.log(error)
-            alert('Could not add commission.!')
-        }
-
+    catch (error) {
+      console.log(error)
+      setToast(TOAST.ERROR(error?.message));
     }
+  }
 
-
-
-    return (
-        <>
-            <ToastMessage show={toast.show} message={toast.message} type={toast.type} handleClose={() => { setToast({ show: false }) }} />
-
-            <div>
-                <div className='page-label'>
-                    <label>Add New Commission</label>
-                </div>
-                <div>
-
-                    <Form className='fcadmin'>
-                    <div class="form-group  form-group5 row">
+  return (
+    <>
+      <ToastMessage show={toast.show} message={toast.message} type={toast.type} handleClose={() => { setToast({ show: false }) }} />
+      <div>
+        <div className='page-label'>
+          <label>Add New Commission</label>
+        </div>
+        <div>
+          <Form className='fcadmin'>
+            <div class="form-group  form-group5 row">
               <label class="col-lg-4 col-form-label float-left GForm_label">Program Name</label>
               <div class="col-lg-5 col-md-2 col-sm-2">
                 <select class="form-control input-lg input-lg1 AEN_border" id="programname" name="Type" type="text"  onChange={(e) => { getProgramDetails(e) }}>
                   <option value={null} selected disabled>Select Program</option>
                   {allprograms.map((item) => {
                     return (
-                      <option value={item._id} data-id={item.fee}>{item.name}</option>
+                      <option value={item._id} id={item.fee}>{item.name}</option>
                     )
                   })}
                 </select>
               </div>
             </div>
-                        <SimpleInputComponent label={'Amount'} placeholder={'Enter Amount'} name={'amount'} id={'amount'} type={'number'} text={commission.amount} readonly/>
-                        <SimpleInputComponent label={'Commission'} placeholder={'Enter Commission'} name={'commission'} id={'commission'} type={'number'} onChange={(e) => { setCommission({ ...commission, commission: e.target.value }) }} />
-                        <SimpleDropDownComponent items={[{ id: 1, option: 'Paid' }, { id: 2, option: 'Not Paid' }]} label={'Payment Status'} name={'payment_status'} id={'payment_status'}  onChange={(e) => { setCommission({ ...commission, payment_status: e.target.value }) }}  />
-                        <button type="submit" className="BTN_ADD_premise " onClick={(e) => handleSubmit(e)} >Add Commission</button>
-
-                    </Form>
-
-                </div>
-            </div>
-        </>
-    )
+            <SimpleInputComponent label={'Amount'} placeholder={'Enter Amount'} name={'amount'} id={'amount'} type={'number'} text={commission.amount} readonly/>
+            <SimpleInputComponent label={'Commission'} placeholder={'Enter Commission'} name={'commission'} id={'commission'} type={'number'} onChange={(e) => { setCommission({ ...commission, commission: e.target.value }) }} />
+            <SimpleDropDownComponent items={[{ id: 1, option: 'Paid' }, { id: 0, option: 'Not Paid' }]} label={'Payment Status'} name={'payment_status'} id={'payment_status'}  onChange={(e) => { setCommission({ ...commission, payment_status: e.target.value }) }}  />
+            <button type="submit" className="BTN_ADD_premise " onClick={(e) => handleSubmit(e)} >Add Commission</button>
+          </Form>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default AddCommission
-
+export default AddCommission;
