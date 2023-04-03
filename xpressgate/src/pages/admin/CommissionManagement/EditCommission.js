78c2cdcfcbedcea1aa575fe2436a34f4033e518d
Keshav Methar <keshav.methar@axzora.com>
2023-04-03 13:09:20 +0530

    Minor changes in Edit Commission

--- xpressgate/src/pages/admin/CommissionManagement/EditCommission.js
+++ xpressgate/src/pages/admin/CommissionManagement/EditCommission.js
@@ -121,7 +121,7 @@ const EditCommission = () => {
             </div>
                         <SimpleInputComponent label={'Amount'} placeholder={'Enter Amount'} name={'amount'} id={'amount'} type={'number'} text={commission.amount} readonly/>
                         <SimpleInputComponent label={'Commission'} placeholder={'Enter Commission'} name={'commission'} id={'commission'} type={'number'} text={commission.commission} onChange={(e) => { setCommission({ ...commission, commission: e.target.value }) }} />
-                        <SimpleDropDownComponent items={[{ id: 1, option: 'Paid' }, { id: 2, option: 'Not Paid' }]} label={'Payment Status'} name={'payment_status'} id={'payment_status'} text={commission.payment_status}   onChange={(e) => { setCommission({ ...commission, payment_status: e.target.value }) }}  />
+                        <SimpleDropDownComponent items={[{ id: 1, option: 'Paid' }, { id: 0, option: 'Not Paid' }]} label={'Payment Status'} name={'payment_status'} id={'payment_status'} text={commission.payment_status}   onChange={(e) => { setCommission({ ...commission, payment_status: e.target.value }) }}  />
                         <button type="submit" className="BUTNnn_ADD_premise " onClick={(e) => handleSubmit(e)}>Update Commission</button>
 
                     </Form>
